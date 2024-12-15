f28d6e26426b434b88a51e2f559e6e4562dc4258
DankExodus <DankExdous@gmail.com>
2024-12-15 04:21:09 -0800

    Add files via upload

--- Engine.cpp
+++ Engine.cpp
@@ -12,23 +12,35 @@ void Engine::input()
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			m_Window.close();
-		}if (event.type == sf::Event::MouseButtonReleased)
+		}
+		if (event.type == sf::Event::MouseButtonReleased)
 		{
 			if (event.mouseButton.button == sf::Mouse::Left)
 			{
-				for (int i = 0; i < 5; i++)
+				if (!failState && scoreCount < 30)
 				{
-					int MIN = 20, MAX = 40;
-					int numPoints = MIN + (rand() % (MAX - MIN + 1));
-					m_particles.push_back(Particle(m_Window, numPoints, { event.mouseButton.x, event.mouseButton.y }));
-					//cout << "I:" << i << endl;
-
-					
-	
+					for (int i = 0; i < 5; i++)
+					{
+						int MIN = 20, MAX = 40;
+						int numPoints = MIN + (rand() % (MAX - MIN + 1));
+						m_particles.push_back(Particle(m_Window, numPoints, { event.mouseButton.x, event.mouseButton.y }));
+					}
 				}
-				//cout << "left click released" << endl;
-			}
+				//cout << "mouse x: " << event.mouseButton.x << endl;
+				//cout << "mouse y: " << event.mouseButton.y << endl;
 
+				//clicked on turkey
+				if (event.mouseButton.x <= bounds.left + bounds.width && event.mouseButton.x >= bounds.left && event.mouseButton.y <= bounds.top + bounds.height && event.mouseButton.y >= bounds.top && !failState  && scoreCount < 30)
+				{
+					scoreCount += 1; 
+					//cout << "Score: " << scoreCount << endl;
+					randY = rand() % 710;
+					turkeyTravel.restart();
+					text = "Score: " + to_string(scoreCount);
+					scoreText.setString(text);
+					oof.play();
+				}
+			}
 		}
 	}
 }
@@ -41,44 +53,91 @@ void Engine::update(float dtAsSeconds)
 			i->update(dtAsSeconds);
 			i++;
 		}
-		else
-		{
-			i = m_particles.erase(i);
-		}
+		else i = m_particles.erase(i);
 	}
-	
-
-
 }
 void Engine::draw() 
 {
-	//gameTexture.loadFromFile("graphics/target.png");
-
-	//gameTarget.setTexture(gameTexture);
-	//gameTarget.setPosition(800, 390);
-
 
 	m_Window.clear();
 
+	m_Window.draw(background);
+	m_Window.draw(Santa);
 	m_Window.draw(gameTarget);
+	m_Window.draw(scoreText);
+	
+	if (failState)
+	{
+		gameOver.setOutlineColor(Color::Red);
+		gameOver.setOutlineThickness(5);
+		m_Window.draw(gameOver);
+
+		santaTexture.loadFromFile("graphics/deadge.png");
+		Santa.setTexture(santaTexture);
+	}
+	if (scoreCount == 30)
+	{
+		gameOver.setString("SANTA IS SAVED!");
+		gameOver.setPosition(264, 412);
+		gameOver.setCharacterSize(145);
+		gameOver.setOutlineColor(Color::Green);
+		gameOver.setOutlineThickness(5);
+		m_Window.draw(gameOver);
+		
+		santaTexture.loadFromFile("graphics/yippie.png");
+		Santa.setTexture(santaTexture);
+	}
+	
 	for (auto& particle : m_particles)
 	{
 		m_Window.draw(particle);
 	}
-
-
-
 	m_Window.display();
 }
 Engine::Engine() 
 {
 	VideoMode vm(VideoMode::getDesktopMode());
-	m_Window.create(vm, "Particles", Style::Default);
+	m_Window.create(vm, "Rafter's Revenge (Particles)", Style::Default);
+
+	scoreCount = 0;
+
+	gameTexture.loadFromFile("graphics/turkey3.png");
+	gameTarget.setTexture(gameTexture);	
+	
+	santaTexture.loadFromFile("graphics/santa2.png");
+	Santa.setTexture(santaTexture);
+	Santa.setPosition(1620, 390);
+
+	gameBackground.loadFromFile("graphics/snow_background_snow_particles.png");
+	background.setTexture(gameBackground);
+
+	text = "Score: " + to_string(scoreCount);
+	font.loadFromFile("font/KIN668.TTF");
+	scoreText.setFont(font);
+	scoreText.setString(text);
+	scoreText.setPosition(1720, 0);
+	scoreText.setFillColor(Color::Black);
+
+	font.loadFromFile("font/KIN668.TTF");
+	gameOver.setFont(font);
+	gameOver.setString("GAME OVER");
+	gameOver.setFillColor(Color::Black);
+	gameOver.setPosition(515, 412);
+	gameOver.setCharacterSize(175);
+
+	buffer.loadFromFile("sound/oof.mp3");
+	oof.setBuffer(buffer);
+    oof.setVolume(10);
+
+	music.openFromFile("sound/rizzmas.mp3");
+	music.setVolume(4);
+	music.play();
+
 }
 void Engine::run() 
 {
 	Clock clock;
-	
+
 	cout << "Starting Particle unit tests..." << endl;
 	Particle p(m_Window, 4, { (int)m_Window.getSize().x / 2, (int)m_Window.getSize().y / 2 });
 	p.unitTests();
@@ -86,13 +145,21 @@ void Engine::run()
 
 	while (m_Window.isOpen())
 	{
-		/*clock.restart();
-		Time time1 = clock.getElapsedTime();
-		dt = time1.asSeconds();*/
 		float dt = clock.restart().asSeconds();
 		input();
 		update(dt);
-		draw();
+		draw();	
+
+		if (bounds.getPosition().x >= stoppingPoint) failState = true;
+		if (!failState && scoreCount != 30)
+		{
+			if (scoreCount < 10) gameTarget.setPosition((turkeyTravel.getElapsedTime().asSeconds() * 1000) - 700, randY);
+			else if (scoreCount >= 10) gameTarget.setPosition((turkeyTravel.getElapsedTime().asSeconds() * 1600) - 700, randY);
+			else if (scoreCount >= 20) gameTarget.setPosition((turkeyTravel.getElapsedTime().asSeconds() * 2500) - 700, randY);
+			else if (scoreCount > 30) gameTarget.setPosition((turkeyTravel.getElapsedTime().asSeconds() * 3000) - 700, randY);
+		}
+
+		bounds = gameTarget.getGlobalBounds();
 	}
 
 }
--- Engine.h
+++ Engine.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 #include "Particle.h"
+#include <sstream>
+
 using namespace sf;
 using namespace std;
 
@@ -18,13 +21,27 @@ private:
 	void update(float dtAsSeconds);
 	virtual void draw();
 
+	float randX = rand() % 1770, randY = (rand() % 710) + 30, stoppingPoint = 1557;
 
-	//Texture gameTexture;
-	//Sprite gameTarget;
+	Texture gameTexture, santaTexture, gameBackground;
+	SoundBuffer buffer, buffer2;
+	Sound oof, yay;
+	Music music;
+	FloatRect bounds;
+	Sprite gameTarget, Santa, background;
+	int scoreCount;
+	Clock turkeyTravel, timer, loseWin;
+	Font font;
+	Text scoreText, gameOver;
+	string text;
+	bool failState;
+
+
+
 
 public:
 	// The Engine constructor
-	Engine();
+	Engine(); //: buffer() {}
 
 	// Run will call all the private functions
 	void run();
